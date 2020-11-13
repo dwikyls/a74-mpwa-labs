a53ffa1d4494ca8810ec31710462d9650fb47f37
dwikyls <kreuzvernicterlanze@gmail.com>
2020-11-13 21:11:35 +0700

    add: service worker

--- service-worker.js
+++ service-worker.js
@@ -1,60 +1,102 @@
-const CACHE_NAME = "firstpwa-v1";
-var urlsToCache = [
-  "/",
-  "/nav.html",
-  "/index.html",
-  "/article.html",
-  "/pages/home.html",
-  "/pages/about.html",
-  "/pages/contact.html",
-  "/css/materialize.min.css",
-  "/js/materialize.min.js",
-  "/manifest.json",
-  "/js/nav.js",
-  "/js/api.js",
-  "/icon.png"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+workbox.precaching.precacheAndRoute([
+    { url: 'index.html', revision: '1' },
+    { url: 'nav.html', revision: '1' },
+    { url: 'js/materialize.min.js', revision: '1' },
+    { url: 'js/nav.js', revision: '1' },
+    { url: 'js/idb.js', revision: '1' },
+    { url: 'js/db.js', revision: '1' },
+    { url: 'js/api.js', revision: '1' },
+    { url: 'sw-regist.js', revision: '1' },
+    { url: 'push.js', revision: '1' },
+    { url: 'css/materialize.min.css', revision: '1' },
+    { url: 'manifest.json', revision: '1' },
+])
 
-self.addEventListener("fetch", function(event) {
-  var base_url = "https://readerapi.codepolitan.com/";
+workbox.routing.registerRoute(
+    new RegExp('pages/'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'pages',
+    }),
+);
 
-  if (event.request.url.indexOf(base_url) > -1) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return fetch(event.request).then(function(response) {
-          cache.put(event.request.url, response.clone());
-          return response;
-        })
-      })
+workbox.routing.registerRoute(
+    new RegExp('article.html'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'article',
+    }),
+);
+
+workbox.routing.registerRoute(
+    new RegExp('match.html'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'match',
+    }),
+);
+
+
+workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'images',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+            }),
+        ],
+    }),
+);
+
+workbox.routing.registerRoute(
+    /^https:\/\/api\.football-data\.org\/v2/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'football-data-API',
+    }),
+);
+
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'google-fonts-stylesheets',
+    }),
+);
+
+
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'google-fonts-webfonts',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200],
+            }),
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries: 30,
+            }),
+        ],
+    }),
+);
+
+self.addEventListener('push', (event) => {
+    let body;
+    if (event.data) {
+        body = event.data.text();
+    } else {
+        body = 'Push message no payload';
+    }
+    let options = {
+        body: body,
+        icon: 'assets/img/icon.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+        }
+    };
+    event.waitUntil(
+        self.registration.showNotification('Push Notification', options)
     );
-  } else {
-    event.respondWith(
-      caches.match(event.request, { ignoreSearch: true }).then(function(response) {
-        return response || fetch (event.request);
-      })
-    )
-  }
-});
-
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
 });
