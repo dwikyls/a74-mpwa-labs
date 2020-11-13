564ebfc78beaa658e7a5d937307273574a172762
dwikyls <kreuzvernicterlanze@gmail.com>
2020-11-13 21:09:55 +0700

    add: indexed database

--- js/db.js
+++ js/db.js
@@ -1,64 +1,129 @@
-var dbPromised = idb.open("news-reader", 1, function(upgradeDb) {
-  var articlesObjectStore = upgradeDb.createObjectStore("articles", {
-    keyPath: "ID"
-  });
-  articlesObjectStore.createIndex("post_title", "post_title", {
-    unique: false
-  });
+let dbPromised = idb.open("team", 1, (upgradeDb) => {
+    if (!upgradeDb.objectStoreNames.contains("articles")) {
+        var teams = upgradeDb.createObjectStore("articles", { keyPath: "id" });
+    }
+    if (!upgradeDb.objectStoreNames.contains("matches")) {
+        var matches = upgradeDb.createObjectStore("matches", { keyPath: "id" });
+    }
+    teams.createIndex("post_title", "post_title", {
+        unique: false
+    });
+    matches.createIndex("post_title", "post_title", {
+        unique: false
+    });
 });
 
-function saveForLater(article) {
-  dbPromised
-    .then(function(db) {
-      var tx = db.transaction("articles", "readwrite");
-      var store = tx.objectStore("articles");
-      console.log(article);
-      store.add(article.result);
-      return tx.complete;
+const saveForLater = (article) => {
+    return new Promise((resolve, reject) => {
+        dbPromised
+            .then((db) => {
+                let tx = db.transaction("articles", "readwrite");
+                tx.objectStore("articles").put(article);
+                return tx;
+            })
+            .then((tx) => {
+                if (tx.complete) {
+                    console.log("Artikel berhasil di simpan.");
+                    resolve(true)
+                } else {
+                    reject(new Error(tx.onerror))
+                }
+            });
     })
-    .then(function() {
-      console.log("Artikel berhasil di simpan.");
+}
+
+const saveMatch = (article) => {
+    return new Promise((resolve, reject) => {
+        dbPromised
+            .then((db) => {
+                let tx = db.transaction("matches", "readwrite");
+                tx.objectStore("matches").put(article);
+                return tx;
+            })
+            .then((tx) => {
+                if (tx.complete) {
+                    console.log("Artikel berhasil di simpan.");
+                    resolve(true)
+                } else {
+                    reject(new Error(tx.onerror))
+                }
+            });
+    })
+}
+
+const getAllTeam = () => {
+    return new Promise((resolve, reject) => {
+        dbPromised
+            .then((db) => {
+                let tx = db.transaction("articles", "readonly");
+                let store = tx.objectStore("articles");
+                return store.getAll();
+            })
+            .then((data) => {
+                if (data !== undefined) {
+                    resolve(data)
+                } else {
+                    reject(new Error("Undefined"))
+                }
+            });
     });
 }
 
-function getAll() {
-  return new Promise(function(resolve, reject) {
-    dbPromised
-      .then(function(db) {
-        var tx = db.transaction("articles", "readonly");
-        var store = tx.objectStore("articles");
-        return store.getAll();
-      })
-      .then(function(articles) {
-        resolve(articles);
-      });
-  });
-}
-
-function getAllByTitle(title) {
-  dbPromised
-    .then(function(db) {
-      var tx = db.transaction("articles", "readonly");
-      var store = tx.objectStore("articles");
-      var titleIndex = store.index("post_title");
-      var range = IDBKeyRange.bound(title, title + "\uffff");
-      return titleIndex.getAll(range);
-    })
-    .then(function(articles) {
-      console.log(articles);
+const getAllMatch = () => {
+    return new Promise((resolve, reject) => {
+        dbPromised
+            .then((db) => {
+                let tx = db.transaction("matches", "readonly");
+                let store = tx.objectStore("matches");
+                return store.getAll();
+            })
+            .then((data) => {
+                if (data !== undefined) {
+                    resolve(data)
+                } else {
+                    reject(new Error("Undefined"))
+                }
+            });
     });
 }
 
-function getById(id) {
-  return new Promise(function(resolve, reject) {
-    dbPromised
-      .then(function(db) {
-        var tx = db.transaction("articles", "readonly");
-        var store = tx.objectStore("articles");
-        return store.get(id);
-      })
-      .then(function(article) {
-        resolve(article);
-      });
-  });
+const deleteById = (id) => {
+    return new Promise((resolve, reject) => {
+        dbPromised
+            .then((db) => {
+                let tx = db.transaction("articles", "readwrite");
+                let store = tx.objectStore("articles");
+                store.delete(id)
+                return tx;
+            })
+            .then((tx) => {
+                if (tx.complete) {
+                    alert('Berhasil dihapus')
+                    resolve(true)
+                } else {
+                    reject(new Error(tx.onerror))
+                }
+            })
+    })
+}
+
+const deleteMatchById = (id) => {
+    return new Promise((resolve, reject) => {
+        dbPromised
+            .then((db) => {
+                let tx = db.transaction("matches", "readwrite");
+                let store = tx.objectStore("matches");
+                store.delete(id)
+                return tx;
+            })
+            .then((tx) => {
+                if (tx.complete) {
+                    alert('Berhasil dihapus')
+                    window.location('')
+                    resolve(true)
+                } else {
+                    reject(new Error(tx.onerror))
+                }
+            })
+    })
 }
